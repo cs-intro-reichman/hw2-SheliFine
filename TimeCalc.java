d16f3fdb146c89d23ace27b55973f01112ad5f85
Sheli Fine <sheli.fine@post.runi.ac.il>
2024-11-15 12:44:26 +0200

    improving if statements

--- TimeCalc.java
+++ TimeCalc.java
@@ -9,25 +9,21 @@ public class TimeCalc {
 // calculate the new time with added minutes
         int TotalMinutes = var1 * 60 + var2 + MinutesToAdd;
         int TotalHours = TotalMinutes / 60;
-        int NewHour = TotalHours % 24;
-        int NewMinutes = TotalMinutes - TotalHours * 60;
+        int newHour = TotalHours % 24;
+        int newMinutes = TotalMinutes - TotalHours * 60;
 
     // prints the time considering adding 0 to the output for the right time format
 
-        if (NewHour > 9) {
-            if (NewMinutes > 9){
-                System.out.println(NewHour + ":" + NewMinutes);
-            } else {
-                System.out.println(NewHour + ":0" + NewMinutes);
-            }
-        } else {
-            if (NewMinutes > 9){
-                System.out.println("0" + NewHour + ":" + NewMinutes);
-        } else {
-            System.out.println("0" + NewHour + ":0" + NewMinutes);
+        if (newHour >= 10 && newMinutes >= 10){
+            System.out.println(newHour + ":" + newMinutes);
+        } else if (newHour >= 10 && newMinutes < 10){
+            System.out.println(newHour + ":0" + newMinutes);
+        } else if (newHour <= 9 && newMinutes >= 10){
+            System.out.println("0" + newHour + ":" + newMinutes);
+        } else if (newHour <= 9 && newMinutes < 10){
+            System.out.println("0" + newMinutes + ":" + newMinutes);
         }
-    }
-}  
+    }  
 }  
 
   
