a7a063d68d30cc37de59ef02393cd4e0deb157e4
Sheli Fine <sheli.fine@post.runi.ac.il>
2024-11-18 18:38:35 +0200

    First try

--- CalcPi.java
+++ CalcPi.java
@@ -4,13 +4,15 @@ public class CalcPi {
 	    int termsNumber = Integer.parseInt(args [0]);
 		double sum = 0;
 
-		for (int i = 1; i <= termsNumber; i++){
-			if (i % 2 == 0){
-				sum += 1/i;
-			} else {
-				sum -= 1/i;
+		for (int i = 1; i <= termsNumber * 2; i += 4){
+			// adds 4 to the denominator for + fractions
+			sum += 1.0 / i;
+			if (i + 2 <= termsNumber * 2){
+				// adds 2 to the denominator for - fractions
+				sum -= 1.0 / (i + 2);
 			}
-			System.out.println(sum);
 		}
+		System.out.println("pi according to Java: " + Math.PI);
+		System.out.println("pi, approximated:     " + sum * 4);
 	}
 }
