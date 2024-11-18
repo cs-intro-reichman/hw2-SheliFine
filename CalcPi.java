5f66d58bbbb6fdf09c6b059f5843d37df55cbc6c
Sheli Fine <sheli.fine@post.runi.ac.il>
2024-11-18 17:51:26 +0200

    another try Chearleading

--- CalcPi.java
+++ CalcPi.java
@@ -1,6 +1,16 @@
 // Computes an approximation of PI.
 public class CalcPi {
 	public static void main(String [] args) { 
-	    // Replace this comment with your code
+	    int termsNumber = Integer.parseInt(args [0]);
+		double sum = 0;
+
+		for (int i = 1; i <= termsNumber; i++){
+			if (i % 2 == 0){
+				sum += 1/i;
+			} else {
+				sum -= 1/i;
+			}
+			System.out.println(sum);
+		}
 	}
 }
